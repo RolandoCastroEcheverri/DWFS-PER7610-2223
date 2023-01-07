5911e86d1733187e83599c78ea7fbb2100da1dff
RolandoCastro <rolandoantonio.castro796@comunidadunir.net>
2023-01-06 22:59:38 -0500

    Actividad5

---
+++ Tema_5/Rolando Castro/5actividad/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './nav/navbar.js';
+import Card from './componentes/cards.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Card />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ Tema_5/Rolando Castro/5actividad/src/componentes/cards.js
@@ -0,0 +1,22 @@
+import '../styles/Cards.css';
+
+export default function Card() {
+    return (
+        <div class="card">
+        <div class="container">
+            <img src="https://brsc.sa.edu.au/wp-content/uploads/2018/09/placeholder-profile-sq.jpg" alt="react logo" style={{ width: '400px', }}/>
+            <h4><b>ROLANDO CASTRO</b></h4>
+            <div class="parent">
+                <div class="centered box">
+                    <p>Ingeniero de sistemas, Estudiante de maestria en desarrollo de software de la Universidad UNIR. Colombiano y en proceso de aprendizaje con esta gran herramienta llamada REACT. Gracias por su apoyo</p>
+                    <p>Gracias por su apoyo</p>
+                </div>
+            </div>
+            <div class="container">
+   
+</div>
+
+        </div>
+      </div> 
+    );
+}
